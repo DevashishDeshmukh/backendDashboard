1633b30c1e4689bed1389418c280931148dcd621
Devashish Deshmukh <devashishadeshmukh@gmail.com>
2023-01-22 15:28:03 +0530

    cjanges

--- app.js
+++ app.js
@@ -148,14 +148,6 @@ app.get("/", (req, res) => {
 });
 
 
-if ( process.env.NODE_ENV == "production"){
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-  }
-
 app.listen(port, ()=>{
     console.log("Server is Running Successfully.")
 });
